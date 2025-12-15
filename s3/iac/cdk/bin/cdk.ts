59b197d21556870e6422a7d924af698406be5d8d
nclbrngs <nicolebringas02@gmail.com>
2025-12-15 08:58:14 +0000

    clean repo and remove terraform artifacts

---
+++ s3/iac/cdk/bin/cdk.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+
+new CdkStack(app, 'CdkStack');
+
+app.synth();
---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,13 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MyBucket', {
+      versioned: true,
+    });
+  }
+}
